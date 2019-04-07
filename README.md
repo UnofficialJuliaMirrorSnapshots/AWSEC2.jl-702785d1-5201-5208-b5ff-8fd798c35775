715a49d07fd97bfa639b89a1634049a437a9a046
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 18:52:38 +0000

    Snapshot of branch master taken on 2019-04-07T14:52:37.537-04:00 from "https://github.com/UnofficialJuliaMirror/AWSEC2.jl-702785d1-5201-5208-b5ff-8fd798c35775"

---
+++ README.md
@@ -0,0 +1,57 @@
+# AWSEC2
+
+AWS EC2 Interface for Julia
+
+[![Build Status](https://travis-ci.org/samoconnor/AWSEC2.jl.svg)](https://travis-ci.org/samoconnor/AWSEC2.jl)
+
+```julia
+using AWSEC2
+
+aws = AWSCore.aws_config()
+
+policy = """{
+    "Version": "2012-10-17",
+    "Statement": [ {
+        "Effect": "Allow",
+        "Action": [ "s3:PutObject", "s3:GetObject" ],
+        "Resource": "arn:aws:s3:::my_bucket/*"
+    } ]
+}"""
+
+init_data = [(
+
+    "cloud_config.txt", "text/cloud-config",
+
+    """packages:
+     - git
+     - gcc
+    """
+
+),(
+
+    "build_julia.sh", "text/x-shellscript",
+
+    """#!/bin/bash
+
+    s3 cp s3://my_bucket/my_code.tgz /tmp
+    tar xzf /tmp/my_code.tgz
+    """
+)]
+
+create_ec2(aws, "my_server",
+                ImageId      = "ami-1ecae776",
+                InstanceType = "c3.large",
+                KeyName      = "ssh-ec2",
+                Policy       = policy,
+                UserData     = init_data)
+
+println("Instance Id: $(ec2_id(aws, "my_server))")
+
+delete_ec2(aws, "my_server")
+
+
+r = ec2(aws, Dict("Action"           => "DescribeImages",
+                  "Filter.1.Name"    => "image-id",
+                  "Filter.1.Value.1" => "ami-1ecae776"))
+println(r)
+```
---
+++ src/AWSEC2.jl
@@ -0,0 +1,214 @@
+#==============================================================================#
+# AWSEC2.jl
+#
+# EC2 API. See http://aws.amazon.com/documentation/ec2/
+#
+# Copyright OC Technology Pty Ltd 2015 - All rights reserved
+#==============================================================================#
+
+
+__precompile__()
+
+
+module AWSEC2
+
+export ec2, ec2_id, delete_ec2, create_ec2, ec2_bash
+
+
+using AWSCore
+using AWSIAM
+using SymDict
+using Retry
+
+
+ec2(aws, action::String; args...) = ec2(aws, action, stringdict(args))
+
+ec2(aws, args::Associative) = ec2(aws, args["Action"], args)
+
+ec2(aws::AWSConfig, action::String, args) = AWSCore.Services.ec2(aws, action, args)
+
+
+function ec2_id(aws::AWSConfig, name)
+
+    r = ec2(aws, "DescribeTags", @SymDict("Filter.1.Name"    = "key",
+                                          "Filter.1.Value.1" = "Name",
+                                          "Filter.2.Name"    = "value",
+                                          "Filter.2.Value.1" = name))
+
+    r = r["tagSet"]
+
+    if r == ""
+        return nothing
+    end
+    return r["item"]["resourceId"]
+end
+
+
+function delete_ec2(aws::AWSConfig, name)
+
+    id = ec2_id(aws, name)
+
+    if id == nothing
+        return
+    end
+
+    ec2(aws, "DeleteTags", @SymDict("ResourceId.1" = id, "Tag.1.Key" = "Name"))
+    ec2(aws, "TerminateInstances", @SymDict("InstanceId.1" = id))
+end
+
+
+function create_ec2(aws::AWSConfig, name; ImageId="ami-1ecae776",
+                                          UserData="",
+                                          Policy="",
+                                          args...)
+
+    if isa(UserData,Array)
+        UserData=base64encode(AWSCore.mime_multipart(UserData))
+    end
+
+    # Delete old instance...
+    @protected try 
+
+        delete_ec2(aws, name)
+
+    catch e
+        @ignore if ecode(e) == "TerminateInstances" end
+    end
+
+    request = @SymDict(ImageId,
+                       UserData,
+                       MinCount="1",
+                       MaxCount="1",
+                       args...)
+
+    # Set up InstanceProfile Policy...
+    if Policy != ""
+
+        @protected try 
+
+            iam(aws, Action = "CreateRole",
+                     Path = "/",
+                     RoleName = name,
+                     AssumeRolePolicyDocument = """{
+                        "Version": "2012-10-17",
+                        "Statement": [ {
+                            "Effect": "Allow",
+                            "Principal": {
+                                "Service": "ec2.amazonaws.com"
+                            },
+                            "Action": "sts:AssumeRole"
+                        } ]
+                     }""")
+
+        catch e
+            @ignore if ecode(e) == "EntityAlreadyExists" end
+        end
+
+        iam(aws, Action = "PutRolePolicy",
+                 RoleName = name,
+                 PolicyName = name,
+                 PolicyDocument = Policy)
+
+        @protected try 
+
+            iam(aws, Action = "CreateInstanceProfile",
+                     InstanceProfileName = name,
+                     Path = "/")
+        catch e
+            @ignore if ecode(e) == "EntityAlreadyExists" end
+        end
+
+
+        @repeat 2 try 
+
+            iam(aws, Action = "AddRoleToInstanceProfile",
+                     InstanceProfileName = name,
+                     RoleName = name)
+
+        catch e
+            @retry if ecode(e) == "LimitExceeded"
+                iam(aws, Action = "RemoveRoleFromInstanceProfile",
+                         InstanceProfileName = name,
+                         RoleName = name)
+            end
+        end
+
+        request[Symbol("IamInstanceProfile.Name")] = name
+    end
+
+    r = nothing
+
+    @repeat 4 try
+
+        # Deploy instance...
+        r = ec2(aws, "RunInstances", request)
+
+    catch e
+        @delay_retry if ecode(e) == "InvalidParameterValue" end
+    end
+
+    r = r["instancesSet"]["item"]
+
+    ec2(aws, "CreateTags", Dict("ResourceId.1" => r["instanceId"],
+                                "Tag.1.Key"    => "Name",
+                                "Tag.1.Value"  => name))
+
+    return r
+end
+
+
+function ec2_bash(aws::AWSConfig, script...;
+                  instance_name = "ec2_bash",
+                  instance_type = "c3.large",
+                  image = "amzn-ami-hvm-2015.09.1.x86_64-gp2",
+                  ssh_key = nothing,
+                  policy = nothing,
+                  packages = [])
+
+    user_data = [(
+
+        "cloud_config.txt", "text/cloud-config",
+
+        "packages:\n$(join([" - $p\n" for p in packages]))"
+
+    ),(
+
+        "ec2_bash.sh", "text/x-shellscript",
+
+        """#!/bin/bash
+
+        set -x
+        set -e
+
+        $(join(script, "\n"))
+
+        shutdown -h now
+        """
+    )]
+
+    # http://docs.aws.amazon.com/lambda/latest/dg/current-supported-versions.html
+    ami = ec2(aws, "DescribeImages", @SymDict("Filter.1.Name" = "owner-alias",
+                                              "Filter.1.Value" = "amazon",
+                                              "Filter.2.Name" = "name",
+                                              "Filter.2.Value" = image))
+
+    request = @SymDict(ImageId = ami["imagesSet"]["item"]["imageId"],
+                       InstanceType = instance_type,
+                       UserData = user_data)
+    if ssh_key != nothing
+        request[:KeyName] = ssh_key
+    end
+    if policy != nothing
+        request[:Policy] = policy
+    end
+
+    create_ec2(aws, instance_name; request...)
+end
+
+end # module AWSEC2
+
+
+
+#==============================================================================#
+# End of file.
+#==============================================================================#
